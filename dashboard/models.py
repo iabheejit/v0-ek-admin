4224ab1f31c388005b5781cef74ab29cd16b4c87
Abheejit <abheejit@Batbook-Pro.local>
2025-05-02 13:44:18 +0530

    Deleted old repo files

--- dashboard/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-# This app doesn't need any models as it primarily serves as a frontend
-# and aggregates data from other apps
--- dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/dashboard/recent-activities/', views.recent_activities, name='recent-activities'),
-]
